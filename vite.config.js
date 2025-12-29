c2f6ba711030c229e2730c36765b4136ac30a266
Monica Coca <mcocapelaz@gmail.com>
2025-12-29 18:59:03 +0100

    fix base path for Git Hub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'build' ? '/nombre-de-tu-nuevo-repo/' : '/',
-    server: {
-      open: '/',
-      watch: {
-        usePolling: true
-      }
-    }
+export default defineConfig({
+  plugins: [react()],
+  base: "/myPersonalPortfolio/",
+  build: {
+    outDir: "docs"
   }
-})
+});
