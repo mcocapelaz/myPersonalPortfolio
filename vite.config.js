9f0f012bec50dd73029366b9e0358a9a5e4562a0
Monica Coca <mcocapelaz@gmail.com>
2025-12-29 19:06:40 +0100

    git hub pages configuration added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/myPersonalPortfolio/",
-  build: {
-    outDir: "docs"
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/myPersonalPortfolio/' : '/',
+    server: {
+      open: '/',
+      watch: {
+        usePolling: true
+      }
+    }
   }
-});
+})
